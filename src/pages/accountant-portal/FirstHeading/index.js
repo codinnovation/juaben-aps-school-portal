673ecc953ef511365e56835625bc0305e1874803
Kwabena Asumadu <codinnovations@gmail.com>
2024-10-03 11:27:05 +0100

    updated accountant portal with new design

--- src/pages/accountant-portal/FirstHeading/index.js
+++ src/pages/accountant-portal/FirstHeading/index.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import { auth } from "../../../lib/firebase";
 import { useRouter } from "next/router";
 import styles from "../../../styles/accountant_portal/firstheading.module.css";
-import Person2Icon from "@mui/icons-material/Person2";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import SettingsIcon from '@mui/icons-material/Settings';
 import withSession from "@/lib/session";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Dashboard from "@mui/icons-material/Dashboard";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import Logout from "@mui/icons-material/Logout";
+
 
 function FirstHeading({ user }) {
   const router = useRouter();
-  const [openModal, setOpenModal] = useState(false);
   const [isButtonClicked, setIsButtonClicked] = useState(false);
+
 
   const handleLogout = async (e) => {
     setIsButtonClicked(true);
@@ -37,65 +42,34 @@ function FirstHeading({ user }) {
       setIsButtonClicked(false);
     }
   };
-
-  const handleOpenModal = () => {
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
+;
 
   return (
     <>
       {isButtonClicked && (
         <>
-          <div className={styles.circle_container}>
-            <div className={styles.circle}></div>
-            <span>Logging out...</span>
+          <div className={styles.loadingContainer}>
+            <Box sx={{ display: "flex" }}>
+              <CircularProgress />
+            </Box>
           </div>
         </>
       )}
-      <div className={styles.container}>
-        <div className={styles.containerItems}>
-          <div className={styles.leftContainer}>
-            <p>NANA AKOSUA AKYAMAA || ADVENTIST PREPARATORY SCHOOL</p>
+      <div className={styles.firstHeaderContainer}>
+        <div className={styles.firstHeaderContent}>
+          <div className={styles.contentOne}>
+            <Dashboard className={styles.icon} />
+            <h1>Accountant Portal</h1>
           </div>
 
-          <div className={styles.rightContainer}>
-            <Person2Icon className={styles.icon1} onClick={handleOpenModal} />
-            <LogoutIcon className={styles.icon2} onClick={handleLogout} />
+          <div className={styles.contentTwo}>
+            <NotificationsNoneIcon className={styles.icon} />
+            <PermIdentityIcon className={styles.icon} />
+            <SettingsIcon className={styles.icon} />
+            <Logout className={styles.icon} onClick={handleLogout} />
           </div>
         </div>
       </div>
-
-      <Dialog open={openModal} onClose={handleCloseModal}>
-        <DialogTitle
-          style={{ fontSize: "15px", textTransform: "uppercase" }}
-        >{`${user?.user?.displayName || ""} Profile`}</DialogTitle>
-        <DialogContent>
-          <ul
-            style={{
-              textDecoration: "none",
-              listStyle: "none",
-              fontFamily: "sans-serif",
-            }}
-          >
-            <li style={{ fontSize: "12px", fontFamily: "sans-serif" }}>
-              Email - {user?.user?.email}
-            </li>
-            <li style={{ fontSize: "12px" }}>
-              Role - {user?.displayName}
-            </li>
-            <li style={{ fontSize: "12px" }}>
-              Created - {user?.user?.metadata?.creationTime}
-            </li>
-            <li style={{ fontSize: "12px" }}>
-              Last Login - {user?.user?.metadata?.lastSignInTime}
-            </li>
-          </ul>
-        </DialogContent>
-      </Dialog>
       <ToastContainer />
     </>
   );
