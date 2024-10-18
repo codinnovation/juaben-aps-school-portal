e11f81956833b5108d6ab756adccdf1cd7cf814c
Kwabena Asumadu <codinnovations@gmail.com>
2024-10-18 11:36:45 +0100

    added menu container

--- src/pages/administrator-portal/firstHeader/index.js
+++ src/pages/administrator-portal/firstHeader/index.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../../styles/admin_portal_css/firstHeading.module.css";
 import withSession from "@/lib/session";
 import EventIcon from '@mui/icons-material/Event';
 import PersonIcon from '@mui/icons-material/Person';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import Image from "next/image";
-import SchoolImage from '../../../../public/logo2.png'
+import SchoolImage from '../../../../public/logo2.png';
+import Link from "next/link";
 
 function FirstHeading({ setSearchQuery, searchQuery }) {
+  const [openMenuContainer, setOpenMenuContainer] = useState(false);
+
   return (
     <>
       <div className={styles.firstHeaderContainer}>
@@ -15,8 +19,13 @@ function FirstHeading({ setSearchQuery, searchQuery }) {
           <div className={styles.schoolLogoContainer}>
             <Image src={SchoolImage} width={900} height={900} alt="" />
           </div>
+
           <div className={styles.searchContainer}>
-            <input placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+            <input
+              placeholder="Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
           </div>
 
           <div className={styles.profileContainer}>
@@ -27,11 +36,33 @@ function FirstHeading({ setSearchQuery, searchQuery }) {
 
             <div className={styles.profilePhoto}>
               <PersonIcon className={styles.icon} />
-              <MenuIcon className={styles.icon2} />
+              {openMenuContainer ? (
+                <CloseIcon
+                  className={styles.icon2}
+                  onClick={() => setOpenMenuContainer(false)}
+                />
+              ) : (
+                <MenuIcon
+                  className={styles.icon2}
+                  onClick={() => setOpenMenuContainer(true)}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
+
+      {openMenuContainer && (
+        <>
+          <div className={styles.menuContainer}>
+            <div className={styles.menuContent}>
+            <Link href="/administrator-portal/">Dashboard</Link>
+              <Link href="/administrator-portal/student-list">Student List</Link>
+              <Link href="/administrator-portal/teachers-list">Teacher&apos;s List</Link>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
